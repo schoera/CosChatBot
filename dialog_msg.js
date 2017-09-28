b392c8f989732b2d1af2d3918ee153186f30c0fd
gmeiser <gordon.meiser@gmail.com>
2017-09-28 21:39:15 +0200

    Textanpassung

--- dialog_msg.js
+++ dialog_msg.js
@@ -1,9 +1,9 @@
 exports.msg = {
   "welcome": "Hallo, Du möchtest Deinen Hausrat versichern? Hierfür brauche ich ein paar Angaben ...",
-  "unclear-user-response": "Ich konnte Deine Antwort leider nicht verstehen",
+  "unclear-user-response": "Ich konnte Deine Antwort leider nicht verstehen. ",
   "user-abort": "ABBRUCH", // TODO
 
   "hr-question-plz": "Hallo! Schön, dass Du Dich für eine Hausratversicherung interessierst; eine wichtige Sache! Wir brauchen lediglich ein paar Angaben von Dir und schon kann es losgehen. Wie lautet Deine Postleitzahl?",
-  "invalid-response-plz": "Bitte gib' Deine fünfstellige Postleitzahl an",
+  "invalid-response-plz": "Bitte gib' Deine fünfstellige Postleitzahl an.",
   "hr-question-wohnflaeche": "Das hat geklappt, vielen Dank! Wie groß ist Deine Wohnfläche in Quadratmetern?",
 }
